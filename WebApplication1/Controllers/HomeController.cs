55f404ed6a821e48e83a6e08169ca57493af5e06
Lailton Soares <lailsoares@hotmail.com>
2022-08-13 18:56:17 -0300

    First and Razor pages rests

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace WebApplication1.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App from C# Course";
+
+            ViewData["Aluno"] = "Lailton Soares";
 
             return View();
         }
